6f734e4ef08c4cf3f2b055bfa1de0164b7f470f6
Tyler Hampton <dr.frankinfurter@gmail.com>
2016-10-18 02:34:40 -0400

    Make so that it isn't dependent on a key and instead relies on iam profile

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var mds = require('mongo-dump-stream'),
     MemoryStream = require('memorystream');
 
 function isValidConfig(config) {
-    return (config.mongodb && config.s3 && config.mongodb.url && config.s3.key && config.s3.secret);
+    return (config.mongodb && config.s3 && config.mongodb.url);
 }
 
 function dumpToS3(config, callback) {
@@ -17,7 +17,7 @@ function dumpToS3(config, callback) {
     var stream = new MemoryStream();
     var urlTokens = config.mongodb.url.split('/');
     var dbName = urlTokens[urlTokens.length - 1];
-    var s3 = new AWS.S3({accessKeyId: config.s3.key, secretAccessKey: config.s3.secret});
+    var s3 = new AWS.S3();
     var tsFormat = config.s3.timestampFormat || 'YYYY-MM-DD_HH:mm:ss';
     var backupTS = moment().format(tsFormat);
     var filename = 'mongo_' + backupTS + '.dmp';
